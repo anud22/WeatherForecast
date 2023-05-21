ec763a9e50164331b1be83952ec12ca2a7975507
anuD <adilbagi22@gmail.com>
2023-05-21 09:59:13 -0700

    Added html for the page

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+`use strict`
+var city = 'Atlanta';
+var cityDetails = {};
+var apiKey = "035cffd3ea47ecfd9f5df831bf486032";
+var baseAPIUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid=" + apiKey;
+var geoAPIUrl = "http://api.openweathermap.org/geo/1.0/direct?q={search}&appid=" + apiKey;
+
+
+
+
+
