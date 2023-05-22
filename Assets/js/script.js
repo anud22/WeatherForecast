a428790c87aeade6ca4d00b80c02c19d7f871458
anuD <adilbagi22@gmail.com>
2023-05-22 16:51:12 -0700

    Fixed the code to display weather details for 5 days

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var city = 'Atlanta';
+
 var apiKey = "035cffd3ea47ecfd9f5df831bf486032";
-var baseAPIUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=Imperial&appid=" + apiKey;
-var geoAPIUrl = "http://api.openweathermap.org/geo/1.0/direct?q={search}&appid=" + apiKey;
-var userForm = $('#user-form');
+$(document).ready(function () {
+    var userForm = $('#user-form');
+var searchHistoryContainer = $('.searchHistory-container');
+   
+
+
 
 var getCityDetails = function (city) {
     return fetch('cityList.json')
@@ -23,44 +26,75 @@ var getAPIResponse = function (response) {
     }
 }
 
+
 var weatherDetails = function (url) {
-
-    var details = [];
     return fetch(url).then(response => getAPIResponse(response)).then(data => {
-        console.log(data.list);
-        var dateSet = new Set();
+        console.log(data);
+        var city = data.city.name;
         var detail = {};
-
-        for (var i = 0; i < data.list.length; ++i) {
+        var details = [];
+        var date = data.list[0].dt_txt.split(" ")[0];
+        detail.date = date;
+        detail.temp = data.list[0].main.temp;
+        detail.humidity = data.list[0].main.humidity;
+        detail.wind = data.list[0].wind.speed;
+        detail.icon = data.list[0].weather[0].icon;
+        details.push(detail);
+        detail = {};
+        for (var i = 1; i < data.list.length; ++i) {
             var date = data.list[i].dt_txt.split(" ")[0];
-            if (!dateSet.has(date)) {
-                if (i == 0) {
-                    detail.date = date;
-                    detail.temp = data.list[i].main.temp;
-                    detail.humidity = data.list[i].main.humidity;
-                    detail.wind = data.list[i].wind.speed;
-                    detail.icon = data.list[i].weather[0].icon;
-                    details.push(detail);
-                    detail = {};
-                } else if (data.list[i].dt_txt.includes('12:00:00')) {
-                    detail.date = date;
-                    detail.temp = data.list[i].main.temp;
-                    detail.humidity = data.list[i].main.humidity;
-                    detail.wind = data.list[i].wind.speed;
-                    detail.icon = data.list[i].weather[0].icon;
-                    details.push(detail);
-                    detail = {};
-                }
+            if (data.list[i].dt_txt.includes('12:00:00')) {
+                detail.date = date;
+                detail.temp = data.list[i].main.temp;
+                detail.humidity = data.list[i].main.humidity;
+                detail.wind = data.list[i].wind.speed;
+                detail.icon = data.list[i].weather[0].icon;
+                details.push(detail);
+                detail = {};
+            }
+
+        }
+        console.log(details);
+        var futureWeather = $('.5day-weather');
+        futureWeather.empty();
+        var currWeather = $('.current-weather');
+        currWeather.empty();
+        currWeather.addClass('border border-dark');
+        var heading = $('#days');
+        heading.text('5-Day Forecast');
+        var iconBaseUrl = "http://openweathermap.org/img/wn/";
+        for (var i = 0; i < details.length; ++i) {
+            var iconUrl = iconBaseUrl + details[i].icon + '.png';
+            var weatherIcon = $('<img>').attr('src', iconUrl);
+            var cityDate = $('<h2>' + city + ' (' + details[i].date + ') </h2>');
+            var date = $('<h2>' + details[i].date + '</h2>');
+            var temp = $('<h5> Temp: ' + details[i].temp + ' \u00B0F</h5>');
+            var wind = $('<h5> Wind: ' + details[i].wind + ' MPH</h5>');
+            var humidity = $('<h5> Humidity: ' + details[i].humidity + ' %</h5>');
+            if (i == 0) {
+                currWeather.append(cityDate);
+                cityDate.append(weatherIcon);
+                currWeather.append(temp);
+                currWeather.append(wind);
+                currWeather.append(humidity);
+            } else {
+                var card = $('<div class="card bg-primary text-white p-1"> </div>');
+                futureWeather.append(card);
+                card.append(date);
+                card.append(weatherIcon);
+                card.append(temp);
+                card.append(wind);
+                card.append(humidity);
+
             }
         }
-        details.push(detail);
-        console.log(details);
-
-    })
-
+        return city;
+    }).then(city => saveAndDisplaySearchHistory(city));
 }
 
 var displayWeather = function (event) {
+    var baseAPIUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=Imperial&appid=" + apiKey;
+    var geoAPIUrl = "http://api.openweathermap.org/geo/1.0/direct?q={search}&appid=" + apiKey;
     event.preventDefault();
     var city = $('#city').val().trim();
     if (!city) {
@@ -90,8 +124,11 @@ var displayWeather = function (event) {
             baseAPIUrl = baseAPIUrl.replace('{lat}', lat).replace('{lon}', lon);
             return baseAPIUrl;
         }).then(url => weatherDetails(url));
+    $('#city').val('');
 
 }
 
 
 userForm.on('submit', displayWeather);
+displaySearchHistory();
+});
